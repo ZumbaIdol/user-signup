6ca71de0a6107cb67b47d10e773afd5430d0ed40
David A. Foote <david.a.foote@gmail.com>
2017-10-02 22:28:54 -0500

    Worked on templates and main, errors not working

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, redirect, render_template
+import string
+import cgi
+import os
 
 
 app = Flask(__name__)
@@ -7,7 +10,7 @@ app.config['DEBUG'] = True
 @app.route("/")
 def index():
     return render_template('signup_form.html')
-#@app.run()
+
 
 
 @app.route('/', methods=['POST'])
@@ -16,7 +19,7 @@ def display_signup_form():
 
 def is_alpha(letter):
     try:
-        str(letter)
+        string.ascii_letters
         return True
     except:
         ValueError
@@ -35,14 +38,14 @@ def signup_form():
     email_error = ''
     return signup_form
 
-    if not is_alpha(username):
+    if not string.ascii_letters(username):
         username_error = 'Not a valid character'
     else:
         username = str(username)
         if username < 3 or username > 20:
             username_error = 'Value out of range (3-20)' 
             
-    if not is_alpha(password) :
+    if not string.ascii_letters(password) :
         password_error = 'Not a valid character'
         password = ''
     else:
@@ -50,7 +53,7 @@ def signup_form():
         if password < 3 or password > 20:
             password_error = 'Value out of range (3-20)'
             password = ''
-    if not is_alpha(confirm_password):
+    if not string.ascii_letters(confirm_password):
         confirm_password_error = 'Not a valid character'
         confirm_password = ''
     else:
@@ -58,7 +61,7 @@ def signup_form():
         if confirm_password < 3 or confirm_password > 20:
             confirm_password_error = 'Passwords must match'
             confirm_password = ''
-    if not is_alpha(email):
+    if not string.printable(email):
         email_error = 'Not a valid character'
     else:
         email = str(email)
@@ -66,7 +69,7 @@ def signup_form():
             email_error = 'Invalid email'
 
     if not username_error and not password_error and not confirm_password_error and not email_error:
-        return welcome_page.html
+        return welcome_page.html + username
 
     else:
         return signup_form(username_error=username_error, password_error=password_error, confirm_password_error=confirm_password_error, email_error=email_error, username=username, password=password, confirm_password=confirm_email_error, email=email)
