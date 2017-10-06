80893f3f7da09e917a95057b155d518c671d52f2
David A. Foote <david.a.foote@gmail.com>
2017-10-05 20:39:17 -0500

    Finished!

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from flask import Flask, request, redirect, render_template
-import string
-import cgi
-import os
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-
-@app.route("/")
-def index():
-    return render_template('signup_form.html')
-
-
 
 @app.route('/signup_form', methods=['POST'])
 def display_signup_form():
@@ -19,53 +9,42 @@ def display_signup_form():
     password = request.form['password']
     confirm_password = request.form['confirm_password']
     email = request.form['email']
-    return username 
+    
     username_error = ''
     password_error = ''
+    confirm_password_error = ''
     confirm_email_error = ''
     email_error = ''
-    return signup_form
     
-    if not string.ascii(username):
-        username_error = 'Not a valid character'
-    else:
-        username = str(username)
-    if username < 3 or username > 20:
+    if username == "" or len(username) < 3 or len(username) > 20:
        username_error = 'Value out of range (3-20)' 
             
-    if not string.ascii(password) :
-        password_error = 'Not a valid character'
-        password = ''
-    else:
-        password = str(password)
-    if password < 3 or password > 20:
+    if password == "" or len(password) < 3 or len(password) > 20:
         password_error = 'Value out of range (3-20)'
         password = ''
-    if not string.ascii(confirm_password):
-        confirm_password_error = 'Not a valid character'
-        confirm_password = ''
-    else:
-        confirm_password = str(confirm_password)
-    if confirm_password < 3 or confirm_password > 20:
+    
+    if confirm_password == "" or len(confirm_password) < 3 or len(confirm_password) > 20:
         confirm_password_error = 'Passwords must match'
         confirm_password = ''
-    if not string.ascii(email):
-        email_error = 'Not a valid character'
-    else:
-        email = str(email)
-    if email < 3 or email > 20:
-        email_error = 'Invalid email'
-
-    if not username_error: 
-        if not password_error:
-            if not confirm_password_error:
-                if not email_error:
-                    return welcome_page.html
-
-    else:
-        return signup_form(username_error=username_error, password_error=password_error, confirm_password_error=confirm_password_error, email_error=email_error, username=username, password=password, confirm_password=confirm_password, email=email)
+    
+    if email != "":
+        if "@" or "." not in email:
+            email_error = 'Invalid entry'
         
+        if len(email) < 3 or len(email) > 20:
+            email_error = 'Invalid email'
+
+    if username_error == "" and password_error == "" and confirm_password_error == "" and email_error == "": 
+                            
+        return render_template('welcome_page.html', username=username)
+
+    else:
+        return render_template('signup_form.html', username_error=username_error, password_error=password_error, confirm_password_error=confirm_password_error, email_error=email_error)
         
+@app.route("/")
+def index():
+    return render_template('signup_form.html')
+       
 
 app.run()
 
